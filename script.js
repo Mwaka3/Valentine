d568a93d325f49e3057530c1725a153de9bdba84
Maxwell <maxwellmwaka3@gmail.com>
2026-01-28 16:08:35 -0500

    ssss

--- script.js
+++ script.js
@@ -62,7 +62,7 @@ const messages = [
     "If you say no, I will be really sad...",
     "I will be very sad...",
     "I will be very very very sad...",
-    "AIII, Shaurii zako😒...",
+    "Aiii, Shaurii zako😒...",
     "Just kidding, say yes please! ❤️"
 ];
 
